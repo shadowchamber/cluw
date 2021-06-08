a4f8d7630a117dc4cfa26fd490027ce2335af8e0
AndriiPylypenkoEpam <70526512+AndriiPylypenkoEpam@users.noreply.github.com>
2021-06-08 11:49:06 +0300

    yarn

---
+++ src/cluw/Wrappers/Npm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cluw.Wrappers
+{
+    public class Npm : WrapperBase
+    {
+        public async Task<CommandResult> InstallYarnAsync()
+        {
+            return await this.RunCommandAsync("npm", "install --global yarn").ConfigureAwait(false);
+        }
+    }
+}
---
+++ src/cluw/Wrappers/Yarn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cluw.Wrappers
+{
+    public class Yarn : WrapperBase
+    {
+        public async Task<CommandResult> YarnAsync()
+        {
+            return await this.RunCommandAsync("yarn").ConfigureAwait(false);
+        }
+    }
+}
