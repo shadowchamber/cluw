9e918f61078510f93dba4e141570b32793901c14
AndriiPylypenkoEpam <70526512+AndriiPylypenkoEpam@users.noreply.github.com>
2021-06-09 10:02:49 +0300

    wass3

---
+++ src/cluw/Wrappers/AwsS3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cluw.Wrappers
+{
+    public class AwsS3 : WrapperBase
+    {
+        public async Task<CommandResult> Copy(string from, string to)
+        {
+            return await this.RunCommandAsync($"cmd /c \"aws --region us-east-1 s3 cp \"{from}\" \"{to}\"").ConfigureAwait(false);
+        }
+    }
+}
